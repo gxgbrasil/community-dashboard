c4cb49fefc3306bebc19e3c8cf551440d11adc3e
anapaulagomes <apgomes88@gmail.com>
2016-03-26 21:51:27 -0300

    Data to use on tests

---
+++ app/tests/unit/syntaxsugar.py
@@ -0,0 +1,76 @@
+#!env python
+# -*- coding: utf-8 -*-
+
+
+class SyntaxSugar:
+
+    @staticmethod
+    def group_by_urlname():
+        return {
+            "id": 17205732,
+            "name": "Google Developers Group - Belo Horizonte",
+            "link": "http://www.meetup.com/GDG-BH/",
+            "urlname": "GDG-BH",
+            "description": "<p>Google Developer Groups (GDG) Belo Horizonte é um grupo para os apaixonados desenvolvedores que usam as tecnologias de Google como Android, Google APIs, GWT, App Engine, dentre outras. Formamos uma comunidade de desenvolvedores com o objetivo de compartilhar conhecimentos e criar projetos interessantes no conceito de fonte aberta e APIs públicas.</p>",
+            "created": 1411653886000,
+            "city": "Belo Horizonte",
+            "country": "BR",
+            "localized_country_name": "Brazil",
+            "state": "",
+            "join_mode": "open",
+            "visibility": "public",
+            "lat": -19.920000076293945,
+            "lon": -43.939998626708984,
+            "members": 1333,
+            "organizer": {
+                "id": 135268292,
+                "name": "Ana Paula Gomes",
+                "bio": "",
+                "photo": {
+                    "id": 252372954,
+                    "highres_link": "http://photos2.meetupstatic.com/photos/member/3/2/9/a/highres_252372954.jpeg",
+                    "photo_link": "http://photos2.meetupstatic.com/photos/member/3/2/9/a/member_252372954.jpeg",
+                    "thumb_link": "http://photos4.meetupstatic.com/photos/member/3/2/9/a/thumb_252372954.jpeg"
+                }
+            },
+            "who": "Google Developers",
+            "group_photo": {
+                "id": 445236893,
+                "highres_link": "http://photos2.meetupstatic.com/photos/event/9/0/1/d/highres_445236893.jpeg",
+                "photo_link": "http://photos2.meetupstatic.com/photos/event/9/0/1/d/600_445236893.jpeg",
+                "thumb_link": "http://photos2.meetupstatic.com/photos/event/9/0/1/d/thumb_445236893.jpeg"
+            },
+            "timezone": "America/Sao_Paulo",
+            "category": {
+                "id": 34,
+                "name": "Tech",
+                "shortname": "Tech",
+                "sort_name": "Tech"
+            },
+            "photos": [
+                {
+                    "id": 446500557,
+                    "highres_link": "http://photos2.meetupstatic.com/photos/event/9/e/6/d/highres_446500557.jpeg",
+                    "photo_link": "http://photos2.meetupstatic.com/photos/event/9/e/6/d/600_446500557.jpeg",
+                    "thumb_link": "http://photos4.meetupstatic.com/photos/event/9/e/6/d/thumb_446500557.jpeg"
+                },
+                {
+                    "id": 446504419,
+                    "highres_link": "http://photos3.meetupstatic.com/photos/event/a/d/8/3/highres_446504419.jpeg",
+                    "photo_link": "http://photos3.meetupstatic.com/photos/event/a/d/8/3/600_446504419.jpeg",
+                    "thumb_link": "http://photos3.meetupstatic.com/photos/event/a/d/8/3/thumb_446504419.jpeg"
+                },
+                {
+                    "id": 446510840,
+                    "highres_link": "http://photos4.meetupstatic.com/photos/event/c/6/9/8/highres_446510840.jpeg",
+                    "photo_link": "http://photos2.meetupstatic.com/photos/event/c/6/9/8/600_446510840.jpeg",
+                    "thumb_link": "http://photos4.meetupstatic.com/photos/event/c/6/9/8/thumb_446510840.jpeg"
+                },
+                {
+                    "id": 447984006,
+                    "highres_link": "http://photos2.meetupstatic.com/photos/event/5/d/c/6/highres_447984006.jpeg",
+                    "photo_link": "http://photos2.meetupstatic.com/photos/event/5/d/c/6/600_447984006.jpeg",
+                    "thumb_link": "http://photos4.meetupstatic.com/photos/event/5/d/c/6/thumb_447984006.jpeg"
+                }
+            ]
+        }
