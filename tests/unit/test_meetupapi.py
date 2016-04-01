1cdbb3777fe03e6e992ed70f062ddac96098bcc8
anapaulagomes <apgomes88@gmail.com>
2016-04-01 01:17:22 -0300

    Use VCR records and verifies if all fields are being returned

--- tests/unit/test_meetupapi.py
+++ tests/unit/test_meetupapi.py
@@ -1,11 +1,16 @@
 from app.meetupapi.connection import MeetupAPI
-from syntaxsugar import SyntaxSugar
+from fixtures import MeetupApiFixtures
 from vcr_unittest import VCRTestCase
 
 
 class TestMeetupAPI(VCRTestCase):
 
-    def test_get_group_information(self):
+    def test_should_get_group_information(self):
         meetup = MeetupAPI()
-        group_info = meetup.get_group('GDG-BH')
-        self.assertEquals(SyntaxSugar.group_by_urlname().keys(), group_info.keys())
+        received_group = meetup.get_group('GDG-BH')
+
+        fields_received_group = sorted(received_group.keys())
+        fields = sorted(MeetupApiFixtures.group_by_urlname().keys())
+
+        for field_fixture, fields_received_group in zip(fields, fields_received_group):
+            self.assertEqual(field_fixture, fields_received_group)
