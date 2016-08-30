4125c88cce06e347b6011bed8b4849ef279fab8a
anapaulagomes <apgomes88@gmail.com>
2016-08-30 01:03:20 -0300

    Refactor test name to a better convention and fix test to receive new parameter

--- tests/functional/test_dashboard.py
+++ tests/functional/test_dashboard.py
@@ -11,12 +11,22 @@ class TestOpenDashboard(unittest.TestCase):
     def tearDown(self):
         self.browser.close()
 
-    def test_if_page_loads(self):
+    def test_should_loads_page(self):
         self.browser.get(self.URL)
         title = self.browser.title
         self.assertIn('Community Dashboard', title)
 
-    def test_if_loads_dashboard(self):
+    def test_should_loads_groupname_as_url_on_dashboard(self):
+        self.browser.get(self.URL)
+        urlname_field = self.browser.find_element_by_id('urlname')
+        urlname_field.send_keys('GDG-BH')
+
+        go_button = self.browser.find_element_by_id('go')
+        go_button.click()
+
+        self.assertIn('http://localhost:5000/dashboard/gdg-bh', self.browser.current_url)
+
+    def test_should_loads_groupname_on_dashboard(self):
         self.browser.get(self.URL)
         urlname_field = self.browser.find_element_by_id('urlname')
         urlname_field.send_keys('GDG-BH')
@@ -27,11 +37,10 @@ class TestOpenDashboard(unittest.TestCase):
         community_name = self.browser.find_element_by_id('community_name')
         self.assertIn('GDG Belo Horizonte', community_name.text)
 
-    def test_when_try_access_inexistent_page(self):
+    def test_should_return_page_not_found_when_try_access_inexistent_page(self):
         self.browser.get(self.URL + 'inexistent')
         page_not_found = self.browser.find_element_by_tag_name('h1')
         self.assertIn('Page Not Found', page_not_found.text)
-
 
 if __name__ == '__main__':
     unittest.main(verbosity=1)
--- tests/unit/test_webapp.py
+++ tests/unit/test_webapp.py
@@ -1,5 +1,6 @@
 import unittest
 from app import app
+import flask
 
 
 class TestApplication(unittest.TestCase):
@@ -7,40 +8,37 @@ class TestApplication(unittest.TestCase):
     def setUp(self):
         self.webapp = app.test_client()
         self.home = self.webapp.get('/')
-        self.dashboard = self.webapp.get('/dashboard')
+        self.urlname = 'GDG-BH'
+        self.dashboard = self.webapp.get('/dashboard/?urlname=' + self.urlname)
 
     def test_home_successful_access(self):
-        self.assertEqual(200, self.home.status_code)
+        assert 200 == self.home.status_code
 
     def test_home_content_type(self):
-        self.assertIn('text/html', self.home.content_type)
+        assert 'text/html' in self.home.content_type
 
     def test_home_content(self):
         page = self.home.data
-        self.assertIn('<title>Community Dashboard</title>', page)
-        self.assertIn('<input type="text" id="urlname" placeholder="urlname">', page)
-        self.assertIn('<a class="ui huge button" href="dashboard">', page)
+        assert '<title>Community Dashboard</title>' in page
 
     def test_dashboard_successful_access(self):
-        self.assertEqual(200, self.dashboard.status_code)
+        assert 200 == self.dashboard.status_code
 
     def test_dashboard_content_type(self):
-        self.assertIn('text/html', self.dashboard.content_type)
+        assert 'text/html' in self.dashboard.content_type
 
     def test_dashboard_content(self):
         page = self.dashboard.data
-        self.assertIn('<h1 id="community-name">GDG Belo Horizonte</h1>', page)
-        self.assertIn('<div class="graphic" id="no-show-chart"></div>', page)
-        self.assertIn('<div class="graphic" id="diversity-chart"></div>', page)
-        self.assertIn('<div class="graphic" id="newest-older-attendees-chart"></div>', page)
-        self.assertIn('<div class="graphic" id="topics-chart"></div>', page)
+        assert '<h1 id="community-name">' + self.urlname + '</h1>' in page
+        assert '<div class="graphic" id="no-show-chart"></div>' in page
+        assert '<div class="graphic" id="diversity-chart"></div>' in page
+        assert '<div class="graphic" id="newest-older-attendees-chart"></div>' in page
+        assert '<div class="graphic" id="topics-chart"></div>' in page
 
     def test_page_not_found(self):
         page_not_found = self.webapp.get("/test")
         self.assertEqual(404, page_not_found.status_code)
 
-    def test_should_return_page_with_group_info(self):
-        group_info_page = self.webapp.get('/group')
-        page = group_info_page.data
-        self.assertEqual(200, group_info_page.status_code)
-        self.assertIn('{\n  "name": "Google Developer Group - Belo Horizonte"\n}', page)
+    def test_should_receive_urlname_from_index_page(self):
+        with app.test_request_context('/dashboard/?urlname=' + self.urlname):
+            assert flask.request.args['urlname'] == self.urlname
