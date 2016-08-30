76a9d3a3468f68c1373b4d6e5678d3d06545cbb0
anapaulagomes <apgomes88@gmail.com>
2016-08-30 01:35:34 -0300

    Fix functional tests and refactor to a clean test name convention

--- tests/functional/test_dashboard.py
+++ tests/functional/test_dashboard.py
@@ -7,6 +7,7 @@ class TestOpenDashboard(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.urlname = 'GDG-BH'
 
     def tearDown(self):
         self.browser.close()
@@ -14,33 +15,32 @@ class TestOpenDashboard(unittest.TestCase):
     def test_should_loads_page(self):
         self.browser.get(self.URL)
         title = self.browser.title
-        self.assertIn('Community Dashboard', title)
+        assert 'Community Dashboard' in title
 
     def test_should_loads_groupname_as_url_on_dashboard(self):
         self.browser.get(self.URL)
         urlname_field = self.browser.find_element_by_id('urlname')
-        urlname_field.send_keys('GDG-BH')
+        urlname_field.send_keys(self.urlname)
 
         go_button = self.browser.find_element_by_id('go')
         go_button.click()
-
-        self.assertIn('http://localhost:5000/dashboard/gdg-bh', self.browser.current_url)
+        assert 'http://localhost:5000/dashboard/?urlname=' + self.urlname in self.browser.current_url
 
     def test_should_loads_groupname_on_dashboard(self):
         self.browser.get(self.URL)
         urlname_field = self.browser.find_element_by_id('urlname')
-        urlname_field.send_keys('GDG-BH')
+        urlname_field.send_keys(self.urlname)
 
         go_button = self.browser.find_element_by_id('go')
         go_button.click()
 
-        community_name = self.browser.find_element_by_id('community_name')
-        self.assertIn('GDG Belo Horizonte', community_name.text)
+        community_name = self.browser.find_element_by_id('community-name')
+        assert self.urlname in community_name.text
 
     def test_should_return_page_not_found_when_try_access_inexistent_page(self):
         self.browser.get(self.URL + 'inexistent')
         page_not_found = self.browser.find_element_by_tag_name('h1')
-        self.assertIn('Page Not Found', page_not_found.text)
+        assert 'Page Not Found' in page_not_found.text
 
 if __name__ == '__main__':
     unittest.main(verbosity=1)
