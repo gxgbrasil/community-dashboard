82b134ce603beb7c186f3263afb4b8063358a35a
Ana Gomes <agomes@thoughtworks.com>
2016-09-02 15:32:05 -0300

    Skip an unused test and fix a method call

--- tests/unit/test_meetupapi.py
+++ tests/unit/test_meetupapi.py
@@ -1,13 +1,15 @@
 from app.meetupapi.connection import MeetupAPI
 from vcr_unittest import VCRTestCase
+import unittest
 
 
 class TestMeetupAPI(VCRTestCase):
 
     #TODO we should not test the api
+    @unittest.skip
     def test_should_get_group_information(self):
         meetup = MeetupAPI()
-        received_group = meetup.get_group('GDG-BH')
+        received_group = meetup.group('GDG-BH')
 
         fields_received_group = received_group.keys()
 
