519925741f62e4202a3550d8a607388e2dcdda9a
Ana Gomes <agomes@thoughtworks.com>
2016-09-05 00:11:59 -0300

    Prepare vcr to work with storage tests

--- tests/unit/test_storage.py
+++ tests/unit/test_storage.py
@@ -1,4 +1,5 @@
 from app.meetupapi.connection import MeetUpAPI
+from app.meetupapi.storage import Storage
 import unittest
 import vcr
 import config
@@ -12,10 +13,11 @@ class TestCaseMeetUpAPI(unittest.TestCase):
                      filter_query_parameters=['key'])
 
     def setUp(self):
+        self.urlname = 'GDG-BH'
         self.meetup = MeetUpAPI()
+        self.storage = Storage()
 
     @my_vcr.use_cassette()
-    def test_should_get_group_information(self):
-        received_group = self.meetup.group('GDG-BH')
-        fields_received_group = received_group.keys()
-        assert 'name' in fields_received_group
+    def test_should_store_group_information(self):
+        group_information = self.meetup.group(self.urlname)
+        response = self.storage.group(group_information)
