41002297a3350303ccbbdb6d5f00a22c5fd537c3
Ana Paula Gomes <apgomes88@gmail.com>
2016-09-16 22:06:25 -0300

    Test and implement filter for rsvp endpoint

--- app/meetupapi/filter.py
+++ app/meetupapi/filter.py
@@ -1,7 +1,9 @@
 class Filter:
 
     def group_basic_info(self, group_data):
-        group_info = {'id': group_data['id'], 'name': group_data['name'], 'members': group_data['members']}
+        group_info = {'id':         group_data['id'],
+                      'name':       group_data['name'],
+                      'created':    group_data['created']}
         return group_info
 
     def events(self, events_data):
@@ -9,6 +11,20 @@ class Filter:
         events_result = events_data['results']
 
         for event in events_result:
-            events_list.append(event['id'])
+            event_details = {'id': event['id'],
+                             'audience': event['yes_rsvp_count'],
+                             'created': event['created'],
+                             'utc_offset': event['utc_offset'],
+                             'duration': event['time']}
+            events_list.append(event_details)
 
         return events_list
+
+    def rsvps(self, rsvps_data):
+        attendees = []
+
+        for rsvp in rsvps_data:
+            if rsvp['response'] == 'yes':
+                attendees.append(rsvp['member']['id'])
+
+        return attendees
--- tests/unit/meetupapi/test_filter.py
+++ tests/unit/meetupapi/test_filter.py
@@ -23,18 +23,34 @@ class TestFilter(unittest.TestCase):
         group_info = self.filter.group_basic_info(group_data)
         assert group_info['id'] == 17205732
         assert group_info['name'] == 'Google Developers Group - Belo Horizonte'
-        assert group_info['members'] == 1556
+        assert group_info['created'] == 1411653886000
         assert len(group_info.keys()) == 3
 
     @my_vcr.use_cassette()
     def test_should_give_list_of_events(self):
         events_data = self.meetup.events(self.urlname)
         events_info = self.filter.events(events_data)
+        first_event = events_info[0]
+
         assert len(events_info) == 57
+        assert first_event['id'] == '218868742'
+        assert first_event['audience'] == 10
+        assert first_event['created'] == 1416824376000
+        assert first_event['utc_offset'] == -7200000
+        assert first_event['duration'] == 1417125600000
 
     @my_vcr.use_cassette()
     def test_should_receive_attendance_status_from_rsvps(self):
-        rsvps_data = self.meetup.rsvps(self.urlname, '218868742')
-        rsvps_attendance_status = self.filter.rsvps(rsvps_data)
+        event_id = '218868742'
+        rsvps_data = self.meetup.rsvps(self.urlname, event_id)
+        rsvps_list = self.filter.rsvps(rsvps_data)
 
-        assert len(rsvps_attendance_status) == 100
+        assert len(rsvps_list) == 8
+
+    @my_vcr.use_cassette()
+    def test_should_receive_attendance_status_from_rsvps_without_no_response(self):
+        event_id = '221804615'
+        rsvps_data = self.meetup.rsvps(self.urlname, event_id)
+        rsvps_list = self.filter.rsvps(rsvps_data)
+
+        assert len(rsvps_list) == 23
