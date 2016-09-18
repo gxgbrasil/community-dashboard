71bcc76483f447e979b21c853467a781c54ff7c8
Ana Paula Gomes <apgomes88@gmail.com>
2016-09-18 12:19:16 -0300

    Store group information and events

--- app/meetupapi/storage.py
+++ app/meetupapi/storage.py
@@ -4,5 +4,37 @@ import redis
 
 class Storage:
 
-    def __init__(self):
+    def __init__(self, test_mode=False):
         self.redis = redis.from_url(os.environ.get("REDIS_URL"))
+
+        #TODO create a configuration file to test environment
+        if test_mode:
+            self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    def group(self, group_information):
+        result = self.redis.set("meetup:GROUP:{0}".format(group_information['urlname']), group_information)
+        return result
+
+    def retrieve_group(self, urlname):
+        result = self.redis.get("meetup:GROUP:{0}".format(urlname))
+        return result
+
+    def events(self, urlname, events_list):
+        pipeline = self.redis.pipeline()
+
+        for event in events_list:
+            pipeline.hset("meetup:EVENTS:{0}:{1}".format(urlname, event['id']), "id", event)
+        result = pipeline.execute()
+
+        return result
+
+    def retrieve_events(self, urlname):
+        result = []
+        for event in self.redis.scan_iter("meetup:EVENTS:{0}:*".format(urlname)):
+            event_details = self.redis.hgetall(event)
+            result.append(event_details)
+
+        return result
+
+    def flush(self):
+        return self.redis.flushall()
--- tests/unit/meetupapi/test_storage.py
+++ tests/unit/meetupapi/test_storage.py
@@ -1,7 +1,64 @@
 import unittest
+from app.meetupapi.storage import Storage
 
 
 class TestStorage(unittest.TestCase):
 
-    def test_should_store_id_name_number_of_members_in_group_information(self):
-        return None
+    @classmethod
+    def setUpClass(self):
+        self.store = Storage(test_mode=True)
+        self.urlname = "GDG-BH-TESTE"
+
+    @classmethod
+    def tearDownClass(self):
+        self.store.flush()
+
+    def test_should_store_group_information(self):
+        group = self.__group_information()
+        result = self.store.group(group)
+        assert result is True
+
+    def test_should_retrieve_group_information(self):
+        group = self.__group_information()
+        self.store.group(group)
+
+        group = self.store.retrieve_group(self.urlname)
+        assert group == str(self.__group_information())
+
+    def test_should_store_events(self):
+        events = self.__events_list()
+        result = self.store.events(self.urlname, events)
+        assert len(result) is 3
+
+    def test_should_retrieve_events_list(self):
+        events = self.__events_list()
+        self.store.events(self.urlname, events)
+
+        events = self.store.retrieve_events(self.urlname)
+        assert type([]) == type(events)
+        assert len(events) == len(self.__events_list())
+
+    def __group_information(self):
+        filtered_group_information = {"id": 17205732,
+                                      "name": "Google Developers Group - Belo Horizonte",
+                                      "urlname": self.urlname,
+                                      "created": 1411653886000}
+        return filtered_group_information
+
+    def __events_list(self):
+        event_details = [{"id": "1886874200",
+                         "audience": 10,
+                         "created": 1416824376000,
+                         "utc_offset": -7200000,
+                         "duration": 1417125600000},
+                         {"id": "1886874211",
+                         "audience": 10,
+                         "created": 1416824376000,
+                         "utc_offset": -7200000,
+                         "duration": 1417125600000},
+                         {"id": "1886874222",
+                         "audience": 10,
+                         "created": 1416824376000,
+                         "utc_offset": -7200000,
+                         "duration": 1417125600000}]
+        return [event_details[0], event_details[1], event_details[2]]
