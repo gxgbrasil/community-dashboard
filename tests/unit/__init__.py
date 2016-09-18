fb5959dc4596dc5f39c22ff0578f37d1bc8f6dc0
Ana Paula Gomes <apgomes88@gmail.com>
2016-09-18 12:42:45 -0300

    Add tests from meetupapi module to suite tests

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,1 +1,3 @@
 from test_webapp import TestApplication
+from meetupapi import TestFilter
+from meetupapi import TestStorage
---
+++ tests/unit/meetupapi/__init__.py
@@ -0,0 +1,2 @@
+from test_filter import TestFilter
+from test_storage import TestStorage
